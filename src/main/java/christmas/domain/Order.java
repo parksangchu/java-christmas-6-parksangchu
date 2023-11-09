1b6466cb923cd05abf9b3e90eeff30ecd2ecdd2a
sangchu <didi1484@gmail.com>
2023-11-10 01:20:19 +0900

    feat: validateMenu 메서드 추가 (메뉴판 확인)

--- src/main/java/christmas/domain/Order.java
+++ src/main/java/christmas/domain/Order.java
@@ -1,11 +1,14 @@
 package christmas.domain;
 
+import static christmas.global.Error.INVALID_ORDER;
+
 public class Order {
     private static final String UNIT = "개";
     private String menu;
     private int count;
 
     public Order(String menu, int count) {
+        validateMenu(menu);
         this.menu = menu;
         this.count = count;
     }
@@ -13,5 +16,15 @@ public class Order {
     @Override
     public String toString() {
         return menu + " " + count + UNIT;
+    }
+
+    private void validateMenu(String menu) {
+        if (isInvalidMenu(menu)) {
+            throw new IllegalArgumentException(INVALID_ORDER.getMessage());
+        }
+    }
+
+    private boolean isInvalidMenu(String menu) {
+        return Menu.from(menu) == null;
     }
 }
