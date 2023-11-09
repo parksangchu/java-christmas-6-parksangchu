6f4d456cf6be95e18bd3387d08011e6ce7a04bf3
sangchu <didi1484@gmail.com>
2023-11-09 17:43:46 +0900

    feat: 메뉴 enum 생성 (메뉴에 따른 가격정보 저장)

--- src/main/java/christmas/domain/Appetizer.java
+++ src/main/java/christmas/domain/Appetizer.java
@@ -1,4 +1,23 @@
 package christmas.domain;
 
 public enum Appetizer {
+    MUSHROOM_SOUP("양송이수프", 6_000),
+    TAPAS("타파스", 5_500),
+    CAESAR_SALAD("시저샐러드", 8_000);
+
+    private final String menu;
+    private final int price;
+
+    Appetizer(String menu, int price) {
+        this.menu = menu;
+        this.price = price;
+    }
+
+    public String getMenu() {
+        return menu;
+    }
+
+    public int getPrice() {
+        return price;
+    }
 }
--- src/main/java/christmas/domain/Dessert.java
+++ src/main/java/christmas/domain/Dessert.java
@@ -1,4 +1,22 @@
 package christmas.domain;
 
 public enum Dessert {
+    CHOCOLATE_CAKE("초코케이크", 15_000),
+    ICE_CREAM("아이스크림", 5_000);
+
+    private final String menu;
+    private final int price;
+
+    Dessert(String menu, int price) {
+        this.menu = menu;
+        this.price = price;
+    }
+
+    public String getMenu() {
+        return menu;
+    }
+
+    public int getPrice() {
+        return price;
+    }
 }
