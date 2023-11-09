f92ff1d55530e63f374b3a247a35bb25cb69cd46
sangchu <didi1484@gmail.com>
2023-11-09 21:12:50 +0900

    feat: EventController 생성

--- src/main/java/christmas/controller/EventController.java
+++ src/main/java/christmas/controller/EventController.java
@@ -1,4 +1,22 @@
 package christmas.controller;
 
+import christmas.domain.Date;
+import christmas.domain.Orders;
+import christmas.view.InputView;
+import christmas.view.OutputView;
+
 public class EventController {
+    public void start() {
+        OutputView.printWelcome();
+        Date date = new Date(InputView.readDate());
+        Orders orders = new Orders(InputView.readOrder());
+        OutputView.printPreview(date);
+        OutputView.printOrderMenu();
+        OutputView.printTotalOrderAmount();
+        OutputView.printGift();
+        OutputView.printBenefitList();
+        OutputView.printTotalBenefitAmount();
+        OutputView.printPaymentAmount();
+        OutputView.printEventBadge();
+    }
 }
