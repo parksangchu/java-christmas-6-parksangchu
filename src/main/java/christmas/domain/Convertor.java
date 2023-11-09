2a92e278c3c71d2d4c331b62b6421e6ec0192901
sangchu <didi1484@gmail.com>
2023-11-09 23:16:20 +0900

    feat: convertToOrders 메서드 추가 (입력값을 받아 Orders 객체 생성)

--- src/main/java/christmas/domain/Convertor.java
+++ src/main/java/christmas/domain/Convertor.java
@@ -1,16 +1,42 @@
 package christmas.domain;
 
+import static christmas.global.Error.INVALID_ORDER;
+
 import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class Convertor {
-    private static final String delimiter = ",";
+    private static final String ORDERS_DELIMITER = ",";
+    private static final String ORDER_DELIMITER = "-";
+    private static final int MENU_INDEX = 0;
+    private static final int COUNT_INDEX = 1;
+    private static final String REGEXP_PATTERN_ORDER = "([가-힣]+)(\\-)(\\d+)";
 
     public static Orders convertToOrders(String input) {
-        List<Order> orders = Arrays.stream(input.split(delimiter))
-                .map(Order::new)
+        List<Order> orders = Arrays.stream(input.split(ORDERS_DELIMITER))
+                .map(Convertor::convertToOrder)
                 .collect(Collectors.toList());
         return new Orders(orders);
     }
+
+    private static Order convertToOrder(String input) {
+        validatePattern(input);
+        String[] orderGroup = input.split(ORDER_DELIMITER);
+        String menu = orderGroup[MENU_INDEX];
+        int count = Integer.parseInt(orderGroup[COUNT_INDEX]);
+        return new Order(menu, count);
+    }
+
+    private static void validatePattern(String input) {
+        if (!isValidPattern(input)) {
+            throw new IllegalArgumentException(INVALID_ORDER.getMessage());
+        }
+    }
+
+    private static boolean isValidPattern(String input) {
+        return Pattern.matches(REGEXP_PATTERN_ORDER, input);
+    }
+
 }
