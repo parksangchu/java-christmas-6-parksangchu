e322391d33532b5bd691b6a88dd20ff28f24dc36
sangchu <didi1484@gmail.com>
2023-11-09 20:25:27 +0900

    feat: Error enum 생성 (Error 메시지 저장)

---
+++ src/main/java/christmas/view/Error.java
@@ -0,0 +1,16 @@
+package christmas.view;
+
+public enum Error {
+    INVALID_DATE("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요."),
+    INVALID_ORDER("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+
+    private final String message;
+
+    Error(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
