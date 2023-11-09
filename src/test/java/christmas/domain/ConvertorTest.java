8e97f3775b0a8646dd39e46d94228012d9866804
sangchu <didi1484@gmail.com>
2023-11-10 01:37:52 +0900

    fix: 테스트 경우의 수 추가

--- src/test/java/christmas/domain/ConvertorTest.java
+++ src/test/java/christmas/domain/ConvertorTest.java
@@ -9,8 +9,8 @@ import org.junit.jupiter.params.provider.ValueSource;
 class ConvertorTest {
 
     @ParameterizedTest
-    @DisplayName("(한글-숫자),(한글-숫자),,, 형식이 아니면 예외 발생")
-    @ValueSource(strings = {"a-1,b-2", "마-1,a-2"})
+    @DisplayName("(한글-숫자) 형식이 아니거나 메뉴에 없으면 예외 발생")
+    @ValueSource(strings = {"a-1,b-2", "마-1,a-2", "돼지바-1"})
     void convertToOrdersTest(String input) {
         assertThatThrownBy(() -> Convertor.convertToOrders(input))
                 .isInstanceOf(IllegalArgumentException.class);
