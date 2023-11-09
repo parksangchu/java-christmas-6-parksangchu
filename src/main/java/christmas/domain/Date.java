353a5ca4f364f39d491f31dd982b14402b8d357a
sangchu <didi1484@gmail.com>
2023-11-09 20:39:19 +0900

    feat: Date 클래스 생성 (1-31 사이의 날짜 검증)

--- src/main/java/christmas/domain/Date.java
+++ src/main/java/christmas/domain/Date.java
@@ -1,4 +1,20 @@
 package christmas.domain;
 
-public class Date implements Menu {
+import static christmas.global.Error.INVALID_DATE;
+
+public class Date {
+    private static final int MIN_DATE = 1;
+    private static final int MAX_DATE = 31;
+    private final int value;
+
+    public Date(int value) {
+        validateRange(value);
+        this.value = value;
+    }
+
+    private void validateRange(int value) {
+        if (value > MAX_DATE || value < MIN_DATE) {
+            throw new IllegalArgumentException(INVALID_DATE.getMessage());
+        }
+    }
 }
