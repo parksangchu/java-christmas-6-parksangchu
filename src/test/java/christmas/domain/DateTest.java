96e35c86768668024f5293098ac0f5e42e9f64a9
sangchu <didi1484@gmail.com>
2023-11-10 12:46:30 +0900

    test: createDayofWeek 테스트 추가

--- src/test/java/christmas/domain/DateTest.java
+++ src/test/java/christmas/domain/DateTest.java
@@ -1,8 +1,12 @@
 package christmas.domain;
 
+import static java.time.DayOfWeek.SUNDAY;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
+import java.time.DayOfWeek;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -13,5 +17,13 @@ class DateTest {
     void createDate(int input) {
         assertThatThrownBy(() -> new Date(input))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("숫자를 입력받아 요일 생성")
+    void createDayOfWeek() {
+        Date date = new Date(31);
+        DayOfWeek dayOfWeek = date.createDayofWeek();
+        assertThat(dayOfWeek).isEqualTo(SUNDAY);
     }
 }
