6f59f7ac939dbe9e5df521067dcb10d8d91b308b
sangchu <didi1484@gmail.com>
2023-11-10 13:00:58 +0900

    feat: isWeekend 메서드 isStarDay 메서드 추가 (주말 여부, 스타 데이 여부 확인)

--- src/main/java/christmas/domain/Date.java
+++ src/main/java/christmas/domain/Date.java
@@ -1,6 +1,9 @@
 package christmas.domain;
 
 import static christmas.global.Error.INVALID_DATE;
+import static java.time.DayOfWeek.FRIDAY;
+import static java.time.DayOfWeek.SATURDAY;
+import static java.time.DayOfWeek.SUNDAY;
 
 import java.time.DayOfWeek;
 import java.time.LocalDate;
@@ -8,6 +11,9 @@ import java.time.LocalDate;
 public class Date {
     private static final int MIN_DATE = 1;
     private static final int MAX_DATE = 31;
+    private static final int THIS_YEAR = 2023;
+    private static final int THIS_MONTH = 12;
+    private static final int CHRISTMAS_DATE = 25;
     private final int value;
 
     public Date(int value) {
@@ -15,8 +21,17 @@ public class Date {
         this.value = value;
     }
 
+    public boolean isWeekend() {
+        DayOfWeek dayOfWeek = createDayOfWeek();
+        return dayOfWeek == FRIDAY || dayOfWeek == SATURDAY;
+    }
+
+    public boolean isStarDay() {
+        return createDayOfWeek() == SUNDAY || value == 25;
+    }
+
     public DayOfWeek createDayOfWeek() {
-        LocalDate localDate = LocalDate.of(2023, 12, value);
+        LocalDate localDate = LocalDate.of(THIS_YEAR, THIS_MONTH, value);
         return localDate.getDayOfWeek();
     }
 
