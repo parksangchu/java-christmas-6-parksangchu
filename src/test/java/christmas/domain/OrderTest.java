17f2369e80a550c6593825c6f1ab43722104aa3e
sangchu <didi1484@gmail.com>
2023-11-10 14:44:11 +0900

    test: countDessert, countMain 메서드 추가

---
+++ src/test/java/christmas/domain/OrderTest.java
@@ -0,0 +1,23 @@
+package christmas.domain;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class OrderTest {
+
+    @Test
+    @DisplayName("디저트가 없으면 0 반환")
+    void countDessert() {
+        Order order = new Order("초코케이크", 1);
+        assertThat(order.countDessert()).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("메인이 있으면 개수 반환")
+    void countMain() {
+        Order order = new Order("바비큐립", 10);
+        assertThat(order.countMain()).isEqualTo(10);
+    }
+}
