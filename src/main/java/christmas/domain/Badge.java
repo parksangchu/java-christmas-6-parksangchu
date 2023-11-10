f7fdf8b3fa384fb55acce55af46973e8bec5509c
sangchu <didi1484@gmail.com>
2023-11-10 19:20:53 +0900

    feat: Badge enum 추가

---
+++ src/main/java/christmas/domain/Badge.java
@@ -0,0 +1,23 @@
+package christmas.domain;
+
+public enum Badge {
+    STAR("별", 5_000),
+    TREE("트리", 10_000),
+    SANTA("산타", 20_000);
+
+    private final String type;
+    private final int amount;
+
+    Badge(String type, int amount) {
+        this.type = type;
+        this.amount = amount;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+}
