1f8f12dd7f65c0fe720d40b1d7320ae6c421b614
sangchu <didi1484@gmail.com>
2023-11-10 15:37:30 +0900

    test: calculateTotalBenefit 메서드 추가

--- src/test/java/christmas/domain/BenefitTest.java
+++ src/test/java/christmas/domain/BenefitTest.java
@@ -1,5 +1,7 @@
 package christmas.domain;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -8,7 +10,9 @@ class BenefitTest {
     @Test
     @DisplayName("혜택의 총합 계산")
     void calculateTotalBenefit() {
-        Date date = new Date(25);
-        Orders orders = Convertor.toOrders("");
+        Date date = new Date(3);
+        Orders orders = Convertor.toOrders("티본스테이크-1,바비큐립-1,초코케이크-2,제로콜라-1");
+        int totalBenefit = Benefit.calculateTotalBenefit(orders, date);
+        assertThat(totalBenefit).isEqualTo(31246);
     }
 }
