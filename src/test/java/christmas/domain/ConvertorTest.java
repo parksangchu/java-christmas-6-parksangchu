baf2d50e08ed9618f2dedd16973e90a1557c6723
sangchu <didi1484@gmail.com>
2023-11-10 12:46:54 +0900

    fix: 테스트 메서드명 변경

--- src/test/java/christmas/domain/ConvertorTest.java
+++ src/test/java/christmas/domain/ConvertorTest.java
@@ -11,7 +11,7 @@ class ConvertorTest {
     @ParameterizedTest
     @DisplayName("(한글-숫자) 형식이 아니거나 메뉴에 없으면 예외 발생")
     @ValueSource(strings = {"a-1,b-2", "마-1,a-2", "돼지바-1"})
-    void convertToOrdersTest(String input) {
+    void convertToOrders(String input) {
         assertThatThrownBy(() -> Convertor.convertToOrders(input))
                 .isInstanceOf(IllegalArgumentException.class);
     }
