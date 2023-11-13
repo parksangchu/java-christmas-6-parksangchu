d35a14556c43f198b9e6169fc0602abf82340da2
sangchu <didi1484@gmail.com>
2023-11-13 14:57:13 +0900

    test: countZero 메서드 추가

--- src/test/java/christmas/domain/OrderTest.java
+++ src/test/java/christmas/domain/OrderTest.java
@@ -1,6 +1,7 @@
 package christmas.domain;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -20,5 +21,12 @@ class OrderTest {
         Order order = new Order("바비큐립", 10);
         int count = order.countMain();
         assertThat(count).isEqualTo(10);
+    }
+
+    @Test
+    @DisplayName("1개 미만 주문시 예외 발생")
+    void countZero() {
+        assertThatThrownBy(() -> new Order("바비큐립", 0))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
