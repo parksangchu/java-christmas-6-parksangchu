f542175dd49b70ec1a2e6d9b7b0afd12c89bd475
sangchu <didi1484@gmail.com>
2023-12-06 17:48:22 +0900

    feat: 시작 메시지 출력 기능 추가

--- src/main/java/christmas/controller/Controller.java
+++ src/main/java/christmas/controller/Controller.java
@@ -1,7 +1,9 @@
 package christmas.controller;
 
+import christmas.view.OutputView;
+
 public class Controller {
     public void start() {
-
+        OutputView.printStartNotice();
     }
 }
--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -1,8 +1,15 @@
 package christmas.view;
 
+import static christmas.global.Constants.THIS_MONTH;
+
 public class OutputView {
+    private static final String START_NOTICE_FORMAT = "안녕하세요! 우테코 식당 %d월 이벤트 플래너입니다.\n";
+
     public static void printError(Exception e) {
         System.out.println(e.getMessage());
     }
 
+    public static void printStartNotice() {
+        System.out.printf(START_NOTICE_FORMAT, THIS_MONTH.getValue());
+    }
 }
