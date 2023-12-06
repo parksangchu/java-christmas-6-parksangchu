b545f79d5ddc4c3cb5479b1d09c0bea1cbb708ba
sangchu <didi1484@gmail.com>
2023-12-06 18:16:29 +0900

    feat: 날짜 입력 기능 추가

--- src/main/java/christmas/controller/Controller.java
+++ src/main/java/christmas/controller/Controller.java
@@ -1,9 +1,22 @@
 package christmas.controller;
 
+import christmas.domain.VisitDay;
+import christmas.view.InputView;
 import christmas.view.OutputView;
 
 public class Controller {
     public void start() {
         OutputView.printStartNotice();
+        VisitDay visitDay = initVisitDay();
+    }
+
+    private VisitDay initVisitDay() {
+        while (true) {
+            try {
+                return new VisitDay(InputView.readVisitDay());
+            } catch (IllegalArgumentException e) {
+                OutputView.printError(e);
+            }
+        }
     }
 }
--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -1,4 +1,24 @@
 package christmas.view;
 
+import static christmas.global.Constants.THIS_MONTH;
+
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
+    private static final String ASK_VISIT_DAY_FORMAT = "%d월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)\n";
+    private static final String NUMBER_REGEX = "[0-9]+";
+    private static final String INVALID_TYPE_ERROR = "[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.";
+
+    public static int readVisitDay() {
+        System.out.printf(ASK_VISIT_DAY_FORMAT, THIS_MONTH.getValue());
+        String input = Console.readLine();
+        validateType(input);
+        return Integer.parseInt(input);
+    }
+
+    private static void validateType(String input) {
+        if (!input.matches(NUMBER_REGEX)) {
+            throw new IllegalArgumentException(INVALID_TYPE_ERROR);
+        }
+    }
 }
