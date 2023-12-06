b1777cab3f82789d7fc8c636f5b22307324eaa81
sangchu <didi1484@gmail.com>
2023-12-06 18:38:30 +0900

    feat: 메뉴 입력 기능 추가

--- src/main/java/christmas/domain/Order.java
+++ src/main/java/christmas/domain/Order.java
@@ -1,4 +1,11 @@
 package christmas.domain;
 
 public class Order {
+    private final Menu menu;
+    private final int count;
+
+    public Order(Menu menu, int count) {
+        this.menu = menu;
+        this.count = count;
+    }
 }
