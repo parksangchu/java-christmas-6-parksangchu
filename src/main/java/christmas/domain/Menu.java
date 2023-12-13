f11f52d6b85f473c60a2d90a9ea81dd9bfd14b2f
sangchu <didi1484@gmail.com>
2023-12-13 14:44:09 +0900

    feat: 메뉴판 생성

--- src/main/java/christmas/domain/Menu.java
+++ src/main/java/christmas/domain/Menu.java
@@ -1,4 +1,34 @@
 package christmas.domain;
 
+import java.util.Arrays;
+
 public enum Menu {
+    MUSHROOM_SOUP("양송이수프", 6_000), TAPAS("타파스", 5_500), CAESAR_SALAD("시저샐러드", 8_000),
+    T_BONE_STEAK("티본스테이크", 55_000), BBQ_RIB("바비큐립", 54_000),
+    SEAFOOD_PASTA("해산물파스타", 35_000), CHRISTMAS_PASTA("크리스마스파스타", 25_000),
+    CHOCO_CAKE("초코케이크", 15_000), ICE_CREAM("아이스크림", 5_000),
+    ZERO_COKE("제로콜라", 3_000), RED_WINE("레드와인", 60_000), CHAMPAGNE("샴페인", 25_000);
+
+    private final String name;
+    private final int price;
+
+    Menu(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public static Menu from(String name) {
+        return Arrays.stream(values())
+                .filter(menu -> menu.name.equals(name))
+                .findAny()
+                .orElseThrow(() -> new IllegalArgumentException(ErrorMessage.ORDER.getMessage()));
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
 }
