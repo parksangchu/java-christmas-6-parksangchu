3def24bfa2878874decd7c2422045ddebe57dbc7
sangchu <didi1484@gmail.com>
2023-12-13 14:58:15 +0900

    feat: 주문 검증 및 생성 기능 추가

--- src/main/java/christmas/domain/Order.java
+++ src/main/java/christmas/domain/Order.java
@@ -1,4 +1,31 @@
 package christmas.domain;
 
+import christmas.util.Util;
+import java.util.List;
+
 public class Order {
+    private static final String ORDER_REGEX = "^[가-힣]+-[1-9][0-9]*$";
+    private final Menu menu;
+    private final int count;
+
+    public Order(String order) {
+        validatePattern(order);
+        List<String> separatedOrder = Util.splitByHyphen(order);
+        this.menu = findMenu(separatedOrder);
+        this.count = findCount(separatedOrder);
+    }
+
+    private void validatePattern(String order) {
+        if (!order.matches(ORDER_REGEX)) {
+            throw new IllegalArgumentException(ErrorMessage.ORDER.getMessage());
+        }
+    }
+
+    private Menu findMenu(List<String> separatedOrder) {
+        return Menu.from(separatedOrder.get(0));
+    }
+
+    private int findCount(List<String> separatedOrder) {
+        return Integer.parseInt(separatedOrder.get(1));
+    }
 }
--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -2,13 +2,13 @@ package christmas.view;
 
 import camp.nextstep.edu.missionutils.Console;
 import christmas.domain.ErrorMessage;
+import christmas.util.Util;
 import java.util.List;
 
 public class InputView {
     private static final String ASK_VISIT_DAY = "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)";
     private static final String NUM_REGEX = "^[0-9]+$";
     private static final String ASK_ORDERS = "주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)";
-    private static final String DELIMITER = ",";
 
     public static int readVisitDay() {
         System.out.println(ASK_VISIT_DAY);
@@ -18,13 +18,14 @@ public class InputView {
     }
 
     private static void validateType(String visitDay) {
-        if (visitDay.matches(NUM_REGEX)) {
+        if (!visitDay.matches(NUM_REGEX)) {
             throw new IllegalArgumentException(ErrorMessage.VISIT_DAY.getMessage());
         }
     }
 
     public static List<String> readOrders() {
         System.out.println(ASK_ORDERS);
-        
+        String input = Console.readLine();
+        return Util.splitByComma(input);
     }
 }
