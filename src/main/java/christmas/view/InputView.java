063d5448b4129e4ae8c80f62ae6a4b792203e981
sangchu <didi1484@gmail.com>
2023-12-13 14:16:56 +0900

    feat: 날짜 입력 기능 추가

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -1,4 +1,23 @@
 package christmas.view;
 
+import camp.nextstep.edu.missionutils.Console;
+import christmas.domain.ErrorMessage;
+
 public class InputView {
+    private static final String ASK_VISIT_DAY = "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)";
+    private static final String NUM_REGEX = "^[0-9]+$";
+
+    public static int readVisitDay() {
+        System.out.println(ASK_VISIT_DAY);
+        String visitDay = Console.readLine();
+        validateType(visitDay);
+        return Integer.parseInt(visitDay);
+    }
+
+    private static void validateType(String visitDay) {
+        if (visitDay.matches(NUM_REGEX)) {
+            throw new IllegalArgumentException(ErrorMessage.VISIT_DAY.getMessage());
+        }
+    }
+
 }
