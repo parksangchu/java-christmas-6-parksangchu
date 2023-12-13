f8ac02b56f108de17ace1216bf6ff667d3c9d961
sangchu <didi1484@gmail.com>
2023-12-13 13:53:36 +0900

    feat: 인사 메시지 출력 기능 추가

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -1,8 +1,14 @@
 package christmas.view;
 
 public class OutputView {
+    private static final String START_NOTICE = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.";
+
     public static void printError(Exception e) {
         System.out.println(e);
     }
 
+    public static void printStartNotice() {
+        System.out.println(START_NOTICE);
+    }
+    
 }
